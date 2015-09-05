5bc69a642b222485be558ab02ca366b925535e3e
David Chatagnier <getsubmerged@gmail.com>
2015-09-04 19:12:28 -0600

    fixed even.rb

--- even.rb
+++ even.rb
@@ -1,20 +1,15 @@
-def is_even_and_divisible_by_five? (n)
+def is_even_and_divisible_by_five?(n)
   rem_when_div_by_5 = n % 5
 
-  if rem_when_div_by_5 == 0 && 5
-    return true
-  else
-    return false
-  end
+  rem_when_div_by_5 == 0 && is_even?(n)
 end
 
-def is_odd? (n)
-  return ! is_even_and_divisible_by_five?(n)
+def is_even?(n)
+  rem_when_div_by_2 = n % 2
+
+  rem_when_div_by_2 == 0
 end
 
-puts "1 is_even_and_divisible_by_five? #{is_even_and_divisible_by_five? (1)}"
-puts "2 is_even_and_divisible_by_five? #{is_even_and_divisible_by_five? (2)}"
-puts "3 is_even_and_divisible_by_five? #{is_even_and_divisible_by_five? (3)}"
-puts "4 is_even_and_divisible_by_five? #{is_even_and_divisible_by_five? (4)}"
-puts "5 is_even_and_divisible_by_five? #{is_even_and_divisible_by_five? (5)}"
-puts "6 is_even_and_divisible_by_five? #{is_even_and_divisible_by_five? (6)}"
+21.times do |number|
+  puts "#{ number } is_even_and_divisible_by_five? #{is_even_and_divisible_by_five?(number)}"
+end
